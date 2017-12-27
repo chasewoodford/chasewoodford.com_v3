dca14272d16e7c0162d985a16419ae8047f1f74d
chawoodf <chase.woodford@verilogue.com>
2017-12-27 11:43:06 -0500

    Add back gulp, images and sass files; consolidate stylesheets.

--- wp-content/themes/v3/footer.php
+++ wp-content/themes/v3/footer.php
@@ -7,16 +7,54 @@
 
 </main><!-- / end page container, begun in the header -->
 
-<footer class="site-footer">
-	<div class="site-info container">
-		
-		<p>Birthed <a href="http://bckmn.com/naked-wordpress" rel="theme">Naked</a> 
-			on <a href="http://wordpress.org" rel="generator">Wordpress</a> 
-			by <a href="http://bckmn.com" rel="designer">Joshua Beckman</a>
-		</p>
-		
-	</div><!-- .site-info -->
-</footer><!-- #colophon .site-footer -->
+<!--<footer class="site-footer">-->
+<!--	<div class="site-info container">-->
+<!--		-->
+<!--		<p>Birthed <a href="http://bckmn.com/naked-wordpress" rel="theme">Naked</a> -->
+<!--			on <a href="http://wordpress.org" rel="generator">Wordpress</a> -->
+<!--			by <a href="http://bckmn.com" rel="designer">Joshua Beckman</a>-->
+<!--		</p>-->
+<!--		-->
+<!--	</div><!-- .site-info -->
+<!--</footer><!-- #colophon .site-footer -->
+
+<!-- Footer -->
+<footer>
+	<div class="rocket"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-10 mx-auto">
+				<ul class="list-inline text-center">
+					<li class="list-inline-item">
+						<a href="#">
+		                  <span class="fa-stack fa-lg">
+		                    <i class="fa fa-circle fa-stack-2x"></i>
+		                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+		                  </span>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="#">
+		                  <span class="fa-stack fa-lg">
+		                    <i class="fa fa-circle fa-stack-2x"></i>
+		                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+		                  </span>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="#">
+		                  <span class="fa-stack fa-lg">
+		                    <i class="fa fa-circle fa-stack-2x"></i>
+		                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+		                  </span>
+						</a>
+					</li>
+				</ul>
+				<p class="copyright text-muted">Copyright &copy; Chase Woodford 2017</p>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <?php wp_footer(); 
 // This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website. 
--- wp-content/themes/v3/header.php
+++ wp-content/themes/v3/header.php
@@ -1,38 +1,32 @@
-<?php
-	/*-----------------------------------------------------------------------------------*/
-	/* This template will be called by all other template files to begin 
-	/* rendering the page and display the header/nav
-	/*-----------------------------------------------------------------------------------*/
-?>
+<?php ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title>
-	<?php bloginfo('name'); // show the blog name, from settings ?> | 
-	<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page ?>
-</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<meta name="description" content="Personal website for Chase Woodford">
+	<meta name="author" content="Chase Woodford">
 
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
-	// so if you want to load other stylesheets,
-	// I would load them with an @import call in your style.css
-?>
+	<title>
+		<?php bloginfo('name'); // show the blog name, from settings ?> |
+		<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page ?>
+	</title>
 
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<?php wp_head(); 
-// This fxn allows plugins, and Wordpress itself, to insert themselves/scripts/css/files
-// (right here) into the head of your website. 
-// Removing this fxn call will disable all kinds of plugins and Wordpress default insertions. 
-// Move it if you like, but I would keep it around.
-?>
+	<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file,
+		// so if you want to load other stylesheets,
+		// I would load them with an @import call in your style.css
+	?>
 
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?>
 </head>
 
 <body 
