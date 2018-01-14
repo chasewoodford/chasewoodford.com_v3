25af95c064d68973debf48d8ac9591ed0ea2dacd
chawoodf <chase.woodford@verilogue.com>
2018-01-14 16:25:03 -0500

    Closes #5. Replace wp_title with single_post/single_cat calls.

--- wp-content/themes/v3/header.php
+++ wp-content/themes/v3/header.php
@@ -75,22 +75,22 @@
 			<div class="row">
 				<div class="col-lg-8 col-md-10 mx-auto">
 					<div class="post-heading">
-						<h1><?php wp_title(''); ?></h1>
+						<h1><?php single_post_title(); ?></h1>
 					</div>
 				</div>
 			</div>
 		</div>
 	</header>
-<?php else : ?>
+<?php elseif ( is_category() ) : ?>
 	<header class="masthead">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-md-10 mx-auto">
 					<div class="page-heading">
-						<h1><?php wp_title(''); ?></h1>
-						<span class="subheading">
-							TODO: Subtitle goes here
-						</span>
+						<h1><?php single_cat_title(); ?></h1>
+<!--						<span class="subheading">-->
+<!--							TODO: Subtitle goes here-->
+<!--						</span>-->
 					</div>
 				</div>
 			</div>
