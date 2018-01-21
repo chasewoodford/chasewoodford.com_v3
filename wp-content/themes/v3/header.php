ef93f6948baa5492cdb46d2ed077ebafdf4d5dce
chawoodf <chase.woodford@verilogue.com>
2018-01-21 12:16:44 -0500

    Fix for broken ico link.

--- wp-content/themes/v3/header.php
+++ wp-content/themes/v3/header.php
@@ -2,12 +2,12 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-1"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-28396921-1"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		gtag('config', 'UA-XXXXXXXX-1');
+		gtag('config', 'UA-28396921-1');
 	</script>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -20,7 +20,7 @@
 		<?php is_front_page() ? bloginfo('description') : wp_title(''); ?>
 	</title>
 
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . "/images/favicon.ico"; ?>" />
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . "/img/favicon.ico"; ?>" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
