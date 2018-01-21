a56e6d757e8bde0b56e31df89b7dff28df5cc1a3
chawoodf <chase.woodford@verilogue.com>
2018-01-21 12:12:45 -0500

    Closes #2. Add profile pic to About page.

--- wp-content/themes/v3/about.php
+++ wp-content/themes/v3/about.php
@@ -3,7 +3,10 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="col-lg-8 col-md-10 mx-auto">
+	<div class="col-12 col-lg-3 offset-lg-1 col-xl-2 offset-xl-1 mt-2 text-center">
+		<img class="rounded-circle mb-3" src="<?php echo get_template_directory_uri() . "/img/profile.png"; ?>"/>
+	</div>
+	<div class="col-12 col-lg-7 col-xl-8">
 
 		<?php if ( have_posts() ) : ?>
 
