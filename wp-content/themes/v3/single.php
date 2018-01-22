add55f30d35081788df509ade00a3324f6fb8758
chawoodf <chase.woodford@verilogue.com>
2018-01-21 20:10:23 -0500

    Closes #8. Add link to next/prev posts.

--- wp-content/themes/v3/single.php
+++ wp-content/themes/v3/single.php
@@ -11,6 +11,24 @@
 
 				<hr/>
 
+				<div id="pagination" class="clearfix">
+                    <?php $prev_post = get_previous_post(false,''); $next_post = get_next_post(false,''); ?>
+
+                    <?php if (!empty( $next_post )): ?>
+						<div class="next-page float-left">
+							<a title="<?php echo $next_post->post_title; ?>" href="<?php echo get_permalink( $next_post->ID ); ?>">&larr;&nbsp;Next Post</a>
+						</div>
+                    <?php endif; ?>
+
+                    <?php if (!empty( $prev_post )): ?>
+						<div class="past-page float-right">
+							<a title="<?php echo $prev_post->post_title; ?>" href="<?php echo get_permalink( $prev_post->ID ); ?>">Prev Post&nbsp;&rarr;</a>
+						</div>
+                    <?php endif; ?>
+		        </div>
+
+	            <hr/>
+
 			<?php endwhile; ?>
 
             <?php if ( comments_open() || '0' != get_comments_number() ) comments_template( '', true ); ?>
