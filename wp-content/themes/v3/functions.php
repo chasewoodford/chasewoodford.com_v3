8e52cebb306e6d7575b661428a78fe75a0f5647a
chawoodf <chase.woodford@verilogue.com>
2018-01-28 14:50:09 -0500

    Closes #6. Add styles for comments.

--- wp-content/themes/v3/functions.php
+++ wp-content/themes/v3/functions.php
@@ -35,3 +35,41 @@ function first_paragraph($content){
     return preg_replace('/<p([^>]+)?>/', '<p$1 class="post-meta">', $content, 1);
 }
 add_filter('the_content', 'first_paragraph');
+
+// Change the comment reply link to use 'Reply to &lt;Author First Name>'
+function add_comment_author_to_reply_link($link, $args, $comment){
+
+    $comment = get_comment( $comment );
+
+    // If no comment author is blank, use 'Anonymous'
+    if ( empty($comment->comment_author) ) {
+        if (!empty($comment->user_id)){
+            $user=get_userdata($comment->user_id);
+            $author=$user->user_login;
+        } else {
+            $author = __('Anonymous');
+        }
+    } else {
+        $author = $comment->comment_author;
+    }
+
+    // If the user provided more than a first name, use only first name
+    if(strpos($author, ' ')){
+        $author = substr($author, 0, strpos($author, ' '));
+    }
+
+    // Replace Reply Link with "Reply to &lt;Author First Name>"
+    $reply_link_text = $args['reply_text'];
+    $link = str_replace($reply_link_text, 'Reply to ' . $author, $link);
+
+    return $link;
+}
+add_filter('comment_reply_link', 'add_comment_author_to_reply_link', 10, 3);
+
+// Customize comment form submit button
+function custom_comment_form_submit_button( $arg ) {
+    $arg['class_submit'] = 'btn btn-secondary my-3';
+    $arg['label_submit'] = 'Submit&nbsp;&rarr;';
+    return $arg;
+}
+add_filter( 'comment_form_defaults', 'custom_comment_form_submit_button' );
