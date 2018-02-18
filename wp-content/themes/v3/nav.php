0658e8c4dcdb651f9885837a1a66fd0257d07fba
chawoodf <chase.woodford@verilogue.com>
2018-02-17 21:27:40 -0500

    Closes #25. Update for nav look on smaller devices. Update to code color display.

--- wp-content/themes/v3/nav.php
+++ wp-content/themes/v3/nav.php
@@ -8,7 +8,7 @@
             <i class="fa fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ml-auto text-right pr-3 pr-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo get_home_url() . "/resources/documents/resume.pdf"; ?>">resume</a>
                 </li>
