9975cfe469fb7a897e520db512e4dc0145c5cc18
chawoodf <chase.woodford@verilogue.com>
2018-03-10 17:03:39 -0500

    Begin work on portfolio page build out for #3.

---
+++ wp-content/themes/v3/work.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Work */ ?>
+
+<?php get_header(); ?>
+
+<div class="row">
+	<div class="col-12 col-lg-7 col-xl-8">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
