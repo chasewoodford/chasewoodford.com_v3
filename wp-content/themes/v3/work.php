6717ac3b3d19f77e9e5475890795354d1a0df47c
chawoodf <chase.woodford@verilogue.com>
2018-03-11 10:52:24 -0400

    #3 Add portfolio post query and begin dynamic build.

--- wp-content/themes/v3/work.php
+++ wp-content/themes/v3/work.php
@@ -4,38 +4,44 @@
 
 <div class="row">
 
-	<?php if ( have_posts() ) : ?>
+    <?php
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'category_name' => 'portfolio', // slug
+        'posts_per_page' => -1, // show all
+        'orderby' => 'date',
+        'order'   => 'DESC'
+    );
+    $work_query = new WP_Query($args);
+    while($work_query->have_posts()) : $work_query->the_post(); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<div class="col-md-4">
-				<div class="mb-4 item item-1">
-					<div class="item-content">
-						<div class="position-relative item-top-content">
-							<div class="bg-white p-3 item-top-content-inner">
-								<a class="small text-uppercase item-title-link" href="#">Project Name</a>
-								<p class="mb-0 text-muted item-description">
-									Lorem ipsum dolor sit amet consectetur adipiscing elit labore et dolore magna aliqua.
-								</p>
-							</div>
+		<div class="col-md-4">
+			<div class="mb-4 item item-1">
+				<div class="item-content">
+					<div class="position-relative item-top-content">
+						<div class="bg-white p-3 w-100 item-top-content-inner">
+							<a class="small text-uppercase item-title-link" href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+							<p class="mb-0 text-muted item-description">
+								<?php $item_description = get_post_meta($post->ID, 'item-description', true); echo $item_description; ?>
+							</p>
 						</div>
-						<div class="bg-white pt-0 px-3 pb-3 item-add-content">
-							<div class="item-add-content-inner">
-								<div class="row">
-									<div class="col-12">
-										<a class="float-left small" href="#">Learn more&nbsp;&raquo;</a>
-										<a class="float-right small" href="#">See it live&nbsp;&raquo;</a>
-									</div>
+					</div>
+					<div class="bg-white pt-0 px-3 pb-3 item-add-content">
+						<div class="item-add-content-inner">
+							<div class="row">
+								<div class="col-12">
+									<a class="float-left small" href="<?php the_permalink() ?>">Learn more&nbsp;&raquo;</a>
+									<a class="float-right small" href="#">See it live&nbsp;&raquo;</a>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 
-		<?php endwhile; ?>
-
-	<?php endif; ?>
+	<?php endwhile; ?>
 
 </div>
 
