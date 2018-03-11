d434b0c58291eb9e1bf45369eaadf285a0fa4152
chawoodf <chase.woodford@verilogue.com>
2018-03-11 11:44:56 -0400

    #3 Add "see it live" logic.

--- wp-content/themes/v3/work.php
+++ wp-content/themes/v3/work.php
@@ -16,14 +16,17 @@
     $work_query = new WP_Query($args);
     while($work_query->have_posts()) : $work_query->the_post(); ?>
 
+        <?php $item_description = get_post_meta($post->ID, 'item-description', true); ?>
+        <?php $see_it_live = get_post_meta($post->ID, 'see-it-live', true); ?>
+
 		<div class="col-md-4">
 			<div class="mb-4 item" style="background-image: url('<?php the_post_thumbnail_url(); ?>')">
 				<div class="item-content">
 					<div class="position-relative item-top-content">
 						<div class="bg-white p-3 w-100 item-top-content-inner">
-							<a class="small text-uppercase item-title-link" href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+							<a class="small text-uppercase item-title-link" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 							<p class="mb-0 text-muted item-description">
-								<?php $item_description = get_post_meta($post->ID, 'item-description', true); echo $item_description; ?>
+								<?php echo $item_description; ?>
 							</p>
 						</div>
 					</div>
@@ -31,8 +34,10 @@
 						<div class="item-add-content-inner">
 							<div class="row">
 								<div class="col-12">
-									<a class="float-left small" href="<?php the_permalink() ?>">Learn more&nbsp;&raquo;</a>
-									<a class="float-right small" href="#">See it live&nbsp;&raquo;</a>
+									<a class="float-left small" href="<?php the_permalink(); ?>">Learn more&nbsp;&raquo;</a>
+									<?php if ( $see_it_live ) : ?>
+										<a class="float-right small" href="<?php echo $see_it_live; ?>">See it live&nbsp;&raquo;</a>
+                                    <?php endif; ?>
 								</div>
 							</div>
 						</div>
